92302a02b79166ea6f82fa619b478fe3bbf7e193
DK <onwood93@gmail.com>
2024-06-19 07:01:24 +0000

    test

---
+++ inference.py
@@ -0,0 +1,10 @@
+import pandas as pd
+# csv 기준 ntu keypoints 쌍으로 input으로 받기 - 32개 프레임 맞춰서 가져오기
+
+# heatmap, flow, crop 진행
+
+# 모델 통과 - DTW
+
+# cosine 유사도 - 더해서 평균
+
+# 최종 similarity와 csv에 있는 AMT score의 correlation 구하기
